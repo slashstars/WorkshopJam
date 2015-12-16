d050edaf389b3cf7cbaab432379fa6af8cd6c207
kanavrov <l.kanavrov@gmail.com>
2015-12-16 15:41:55 +0200

    Adding platform prefabs and a test scene with arranged platforms.

---
+++ Assets/Scripts/Initialize.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Initialize : MonoBehaviour {
+
+    public Transform SmallPlatformPrefab;
+    public Transform MediumPlatformPrefab;
+    public Transform LargePlatformPrefab;
+
+    private Vector3 BasePlatformPosition = new Vector3(-0.805f, -0.5f, 0);
+    private Vector3 LeftPlatformPosition = new Vector3(-1.115f, -0.05f, 0);
+    private Vector3 RightPlatformPosition = new Vector3(0.79f, -0.05f, 0);
+    private Vector3 MiddlePlatformPosition = new Vector3(-0.161f, 0.29f, 0);
+
+    // Use this for initialization
+    void Awake () {
+        Instantiate(SmallPlatformPrefab, MiddlePlatformPosition, transform.rotation);
+        Instantiate(SmallPlatformPrefab, LeftPlatformPosition, transform.rotation);
+        Instantiate(SmallPlatformPrefab, RightPlatformPosition, transform.rotation);
+        Instantiate(LargePlatformPrefab, BasePlatformPosition, transform.rotation);
+    }
+}
